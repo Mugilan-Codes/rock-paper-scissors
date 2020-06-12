0011f776acd703c46b456852adae93e2d22cf3b7
Mugilan <mugilancodes@gmail.com>
2020-06-12 21:32:23 +0530

    Package.json

---
+++ src/app/script.js
@@ -0,0 +1,48 @@
+const GAME_MOVES = [ROCK, PAPER, SCISSORS];
+
+let round, playerScore, computerScore, realRounds, winner;
+
+console.clear();
+
+const computerPlay = () => {
+  const randomNumber = Math.floor(Math.random() * GAME_MOVES.length);
+
+  return GAME_MOVES[randomNumber];
+};
+
+const playRound = (playerSelection, computerSelection = computerPlay()) => {
+  playerSelection = playerSelection.toUpperCase();
+  switch (playerSelection) {
+    case 'R':
+      playerSelection = ROCK;
+      break;
+    case 'P':
+      playerSelection = PAPER;
+      break;
+    case 'S':
+      playerSelection = SCISSORS;
+      break;
+    default:
+      playerSelection = playerSelection;
+  }
+  if (!GAME_MOVES.includes(playerSelection)) {
+    return `Invalid Move. You Entered ${playerSelection}`;
+  }
+
+  if (playerSelection === computerSelection) {
+    console.log(`It's a Tie! You Both Chose ${playerSelection}`);
+    return 'T';
+  }
+
+  if (
+    (playerSelection === SCISSORS && computerSelection === PAPER) ||
+    (playerSelection === PAPER && computerSelection === ROCK) ||
+    (playerSelection === ROCK && computerSelection === SCISSORS)
+  ) {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 'U';
+  }
+
+  console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  return 'C';
+};
---
+++ src/app/types.js
@@ -0,0 +1,5 @@
+export const ROCK = 'ROCK';
+export const PAPER = 'PAPER';
+export const SCISSORS = 'SCISSORS';
+export const PLAYER = 'PLAYER';
+export const COMPUTER = 'COMPUTER';
