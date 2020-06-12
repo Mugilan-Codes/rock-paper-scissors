fb5f1bf518650031414fcf746f3077f5a4ea5fd9
Mugilan <mugilancodes@gmail.com>
2020-06-12 11:54:52 +0530

    Modify Game function

--- js/script.js
+++ js/script.js
@@ -49,46 +49,47 @@ const playRound = (playerSelection, computerSelection = computerPlay()) => {
   return 'C';
 };
 
-// const game = (maxRounds = 5) => {
-//   if (maxRounds < 0) maxRounds = 5;
-//   if (maxRounds % 2 === 0) maxRounds += 1;
+const game = (maxRounds = 5) => {
+  if (maxRounds < 0) maxRounds = 5;
+  if (maxRounds % 2 === 0) maxRounds += 1;
 
-//   let playerSelection;
-//   let round = 0;
-//   let playerScore = 0;
-//   let computerScore = 0;
-//   let winningScore = Math.floor(maxRounds / 2) + 1;
+  let playerSelection;
+  let round = 0;
+  let playerScore = 0;
+  let computerScore = 0;
+  let winningScore = Math.floor(maxRounds / 2) + 1;
+  let realRounds = 0;
 
-//   console.log({ maxRounds, winningScore });
+  console.log({ maxRounds, winningScore });
 
-//   while (round < maxRounds) {
-//     if (playerScore === winningScore || computerScore === winningScore) break;
+  while (round < maxRounds) {
+    if (playerScore === winningScore || computerScore === winningScore) break;
 
-//     playerSelection = prompt(
-//       'Choose One of the Three \nRock - r\nPaper - p\nScissors - s'
-//     );
+    playerSelection = prompt(
+      'Choose One of the Three \nRock - r\nPaper - p\nScissors - s'
+    );
 
-//     playerSelection = playerSelection.toUpperCase();
+    const roundResult = playRound(playerSelection);
 
-//     switch (playerSelection) {
-//       case 'R':
-//         playerSelection = ROCK;
-//         break;
-//       case 'P':
-//         playerSelection = PAPER;
-//         break;
-//       case 'S':
-//         playerSelection = SCISSORS;
-//         break;
-//       default:
-//         playerSelection = playerSelection;
-//     }
+    switch (roundResult) {
+      case 'T':
+        realRounds++;
+        console.log({ round, playerScore, computerScore, realRounds });
+        continue;
+      case 'U':
+        realRounds++;
+        playerScore++;
+        round++;
+        console.log({ round, playerScore, computerScore, realRounds });
+        break;
+      case 'C':
+        realRounds++;
+        computerScore++;
+        round++;
+        console.log({ round, playerScore, computerScore, realRounds });
+        break;
+    }
+  }
 
-//     if (!GAME_MOVES.includes(playerSelection)) {
-//       console.log(`Invalid Move. You Entered ${playerSelection}`);
-//       continue;
-//     }
-
-//     console.log(playRound(playerSelection));
-//   }
-// };
+  console.log(`The Winner is and it took ${realRounds} to get a result`);
+};
