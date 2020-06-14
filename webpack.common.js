1e56af56d8f0f50246af1063887d3d1f31974d7a
Mugilan <mugilancodes@gmail.com>
2020-06-14 23:07:59 +0530

    Minify CSS/HTML/JS

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,13 +6,8 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({ template: './src/template.html' })],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
       {
         test: /\.html$/,
         use: ['html-loader'],
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,17 @@
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ template: './src/template.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 });
