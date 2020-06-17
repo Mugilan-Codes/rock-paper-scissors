2044e41dcc01a681ceadf85873c990d7ae6c7110
Mugilan <mugilancodes@gmail.com>
2020-06-17 22:19:01 +0530

    Play Round with Images

--- src/app/components/play-round.js
+++ src/app/components/play-round.js
@@ -1,10 +1,27 @@
 import { getComputerChoice } from '../utils/get-comp-choice';
 
+import r from '../../assests/rock_1.png';
+import p from '../../assests/paper_1.png';
+import s from '../../assests/scissors_1.png';
+
+const playerChoice__img = document.querySelector(
+  '.display-options .player-choice img'
+);
+const computerChoice__img = document.querySelector(
+  '.display-options .computer-choice img'
+);
+
 export const playRound = (
   playerChoice,
   computerChoice = getComputerChoice()
 ) => {
   console.log({ playerChoice, computerChoice });
+
+  const pImg = playerChoice === 'r' ? r : playerChoice === 'p' ? p : s;
+  playerChoice__img.src = pImg;
+
+  const cImg = computerChoice === 'r' ? r : computerChoice === 'p' ? p : s;
+  computerChoice__img.src = cImg;
 
   switch (playerChoice + computerChoice) {
     case 'rs':
