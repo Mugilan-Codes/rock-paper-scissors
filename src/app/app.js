ec89bc770709d88829eee6d784f5c4946bc9e0a0
Mugilan <mugilancodes@gmail.com>
2020-06-17 20:52:34 +0530

    Start Game functionality

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,21 @@
-export const game = () => {
-  console.log('Refresh');
+const maxScore__label = document.querySelector('.max-score__input');
+const startGame__button = document.querySelector('.info button');
+const maxRoundsDisplay__p = document.querySelector('.info .rounds');
+const winScoreDisplay__p = document.querySelector('.info .win-score');
+
+export const game = (maxRounds = 5) => {
+  maxRounds % 2 === 0 && maxRounds++;
+
+  const winScore = Math.floor(maxRounds / 2) + 1;
+
+  console.log({ maxRounds, winScore });
+
+  maxScore__label.classList.add('hide');
+  startGame__button.classList.add('hide');
+
+  maxRoundsDisplay__p.classList.remove('hide');
+  maxRoundsDisplay__p.querySelector('span').textContent = maxRounds;
+
+  winScoreDisplay__p.classList.remove('hide');
+  winScoreDisplay__p.querySelector('span').textContent = winScore;
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { game } from './app/app';
 import './main.scss';
 
-game();
+const maxRounds__input = document.querySelector('.info input');
+const startGame__button = document.querySelector('.info button');
+
+startGame__button.addEventListener('click', () => {
+  const maxRounds = maxRounds__input.value;
+
+  maxRounds < 0 ? game() : game(maxRounds);
+});
